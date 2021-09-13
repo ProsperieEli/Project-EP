9e02116aa6d968c6af3aa0785c4253297035c9ea
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-13 15:10:52 -0700

    add data model

---
+++ universal/data.js
@@ -0,0 +1,117 @@
+export const user = {
+    Name: '',
+    YoungChildren: false,
+    Dependents: 0,
+    Adults: 0,
+    Seniors: 0,
+    // HouseholdTotal: (user.Dependents),
+    Salary: 0,
+    Pets: false,
+    Savings: 0,
+    HaveTransport: false,
+    PrescriptionMed: false,
+    PrescriptionEye: false,
+    Assets: false,
+    ItemsNeeded: [],
+    ItemsSuggested: [],
+    // stretch
+    // location
+};
+
+export const neededItems = [{
+    id: 1,
+    name: 'water',
+    needed: true,
+    description: 'Water',        
+}, {
+    id: 2,
+    name: 'food',
+    needed: true,
+    description: 'Food'        
+}, {
+    id: 3,
+    name: 'radio',
+    needed: true,
+    description: 'Battery-Powered (or hand crank) Radio'        
+}, {
+    id: 4,
+    name: 'flashlight',
+    needed: true,
+    description: 'Flashlight'        
+}, {
+    id: 5,
+    name: 'first-aid',
+    needed: true,
+    description: 'First-Aid Kit'        
+}, {
+    id: 6,
+    name: 'shoes',
+    needed: true,
+    description: 'Pair of Shoes'        
+}, {
+    id: 7,
+    name: 'whistle',
+    needed: true,
+    description: 'Whistle'        
+}, {
+    id: 8,
+    name: 'batteries',
+    needed: true,
+    description: 'Extra Batteries'        
+}, {
+    id: 9,
+    name: 'dust-mask',
+    needed: true,
+    description: 'Dust Mask'        
+},{
+    id: 10,
+    name: 'sheeting',
+    needed: true,
+    description: 'Plastic Sheeting'        
+}, {
+    id: 11,
+    name: 'duct-tape',
+    needed: true,
+    description: 'Duct Tape'        
+}, {
+    id: 12,
+    name: 'wipes',
+    needed: true,
+    description: 'Moist Towelettes, Garbage Bags, Plastic Ties'        
+}, {
+    id: 13,
+    name: 'tools',
+    needed: true,
+    description: 'Wrench and/or Pliers'        
+}, {
+    id: 14,
+    name: 'can-opener',
+    needed: true,
+    description: 'Manual Can Opener'        
+}, {
+    id: 15,
+    name: 'map',
+    needed: true,
+    description: 'Local Maps'        
+}, {
+    id: 16,
+    name: 'cell-phone',
+    needed: true,
+    description: 'Cell phone (with chargers and a backup battery)'        
+}]
+
+// export const       
+        
+//         Prescription eyeglasses and contact lens solution
+//         Infant formula, bottles, diapers, wipes and diaper rash cream
+//         Pet food and extra water for your pet
+//         Cash or traveler's checks
+//         Important family documents such as copies of insurance policies, identification and bank account records saved electronically or in a waterproof, portable container
+//         Sleeping bag or warm blanket for each person
+//         Complete change of clothing appropriate for your climate and sturdy shoes
+//         Fire extinguisher
+//         Matches in a waterproof container
+//         Feminine supplies and personal hygiene items
+//         Mess kits, paper cups, plates, paper towels and plastic utensils
+//         Paper and pencil
+//         Books, games, puzzles or other activities for children}
