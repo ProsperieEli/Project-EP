2bada0c1fae0cabada3875738407fe4328998c08
EmilyDSarani <emily.sarani@gmail.com>
2021-09-14 15:45:22 -0500

    no nulls when you submit

--- form-page/usercreate.js
+++ form-page/usercreate.js
@@ -1,16 +1,17 @@
+
 function userCreate(form){
     const userinput = {
 
         youngChildren: form.get('under-3'),
-        dependents: form.get('kids'),
-        adults: form.get('adults'),
-        seniors: form.get('seniors'),
+        dependents: Number(form.get('kids')),
+        adults: Number(form.get('adults')),
+        seniors: Number(form.get('seniors')),
         salary: form.get('salary'),
         pets: form.get('pets'),
         savings: form.get('emergency-funds'),
-        haveTransport: form.get('havetransport'),
-        prescriptionMed: form.get('prescriptionmed'),
-        prescriptionEye: form.get('prescriptioneye'),
+        haveTransport: form.get('transportation'),
+        prescriptionMed: form.get('prescription'),
+        prescriptionEye: form.get('eyewear'),
         assets: form.get('assets'),
     };
     return userinput;
