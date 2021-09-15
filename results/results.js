d3b2867247fe631c6b0f84a0dbef9b6f325ea9f1
ProsperieEli <ElijahProsperie@gmail.com>
2021-09-15 17:09:56 -0400

    checking

--- results/results.js
+++ results/results.js
@@ -15,3 +15,175 @@
     //get list
     //pie graph(50% necessity) 50% need.
     //Any items not marked as "true" return here in list area.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { getEssentialsList, getSuggestedList } from '../universal/local-storage-utils.js';
+
+
+const essentialList = getEssentialsList();
+    .filter(essentials => essentials.items)
+
+
+
+
+
+
+   
+const ctx = document.getElementById('myChart');
+const myChart = new Chart(ctx, 
+    //eslint-disable-line 
+    {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+ 
--- universal/local-storage-utils.js
+++ universal/local-storage-utils.js
@@ -57,5 +57,4 @@ export function addCompletedProp() {
 
     setEssentials(essentials);
     setSuggested(suggested);
-
 }
