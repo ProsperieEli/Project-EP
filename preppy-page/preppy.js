744369c25710b72636ae4d8d3a990d78e6322f99
EmilyDSarani <emily.sarani@gmail.com>
2021-09-15 19:23:38 -0500

    what why

--- preppy-page/preppy.js
+++ preppy-page/preppy.js
@@ -93,76 +93,70 @@ const getpiebutton = document.getElementById('go-to-results');
 getpiebutton.addEventListener('click', () => {
     window.location.href = '../results/index.html';
 });
-// - add completed: false property to each item
-// - render each essential item
-// - render each suggested
-//     - checking if need === true before rendering
-
-// - make each rendered item clickable
-//     - completed: true on click
-//     - toggleable? stretch goal
 
 
 
-//// progress bar animation and config from Chart.js ////
-// eslint-disable-next-line no-unused-vars
-// const config = {
-//     type: 'line',
-//     data: essentialItemsList,
-//     options: {
-//         animation: {
-//             duration: 2000,
-//             onProgress: function(context) {
-//                 if (context.initial) {
-//                     initProgress.value = context.currentStep / context.numSteps;
-//                 } else {
-//                     progress.value = context.currentStep / context.numSteps;
-//                 }
-//             },
-//             onComplete: function(context) {
-//                 if (context.initial) {
-//                     console.log('Initial animation finished');
-//                 } else {
-//                     console.log('animation finished');
-//                 }
-//             }
-//         },
-//         interaction: {
-//             mode: 'nearest',
-//             axis: 'x',
-//             intersect: false
-//         },
-//         plugins: {
-//             title: {
-//                 display: true,
-//                 text: 'Chart.js Line Chart - Animation Progress Bar'
-//             }
-//         },
-//     },
-// };
 
-// const initProgress = document.getElementById('initialProgress');
-// const progress = document.getElementById('animationProgress');
 
-// here is where we need to update the data being pulled, to pull from the User object
-// const DATA_COUNT = 7;
-// const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
+// progress bar animation and config from Chart.js ////
 
-// const labels = Utils.months({ count: 7 });
-// const data = {
-//     labels: labels,
-//     datasets: [
-//         {
-//             label: 'Dataset 1',
-//             data: Utils.numbers(NUMBER_CFG),
-//             borderColor: Utils.CHART_COLORS.red,
-//             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-//         },
-//         {
-//             label: 'Dataset 2',
-//             data: Utils.numbers(NUMBER_CFG),
-//             borderColor: Utils.CHART_COLORS.blue,
-//             backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-//         }
-//     ]
-// };
+const config = {
+    type: 'line',
+    data: essentialItemsList,
+    options: {
+        animation: {
+            duration: 2000,
+            onProgress: function(context) {
+                if (context.initial) {
+                    initProgress.value = context.currentStep / context.numSteps;
+                } else {
+                    progress.value = context.currentStep / context.numSteps;
+                }
+            },
+            onComplete: function(context) {
+                if (context.initial) {
+                    console.log('Initial animation finished');
+                } else {
+                    console.log('animation finished');
+                }
+            }
+        },
+        interaction: {
+            mode: 'nearest',
+            axis: 'x',
+            intersect: false
+        },
+        plugins: {
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart - Animation Progress Bar'
+            }
+        },
+    },
+};
+
+const initProgress = document.getElementById('initialProgress');
+const progress = document.getElementById('animationProgress');
+
+//here is where we need to update the data being pulled, to pull from the User object
+const DATA_COUNT = 7;
+const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
+
+const labels = Utils.months({ count: 7 });
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: Utils.numbers(NUMBER_CFG),
+            borderColor: Utils.CHART_COLORS.red,
+            backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+        },
+        {
+            label: 'Dataset 2',
+            data: Utils.numbers(NUMBER_CFG),
+            borderColor: Utils.CHART_COLORS.blue,
+            backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+        }
+    ]
+};
