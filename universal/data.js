d21ef38dc92a6243fb12180c95eecee9f423dd8b
Zack Lyon <zack.j.lyon@gmail.com>
2021-09-16 13:16:06 -0700

    Add placeholder links in data file

--- universal/data.js
+++ universal/data.js
@@ -30,7 +30,7 @@ export const neededItems = [{
     amountPerPerson: 1,
     numDays: 3,
     consumables: `gallons`,
-
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'
 }, {
     id: 2,
     name: 'food',
@@ -38,37 +38,44 @@ export const neededItems = [{
     description: `Have 3 meals per person for up to 3 days`,
     amountPerPerson: 3,
     numDays: 3,
-    consumables: `meals`,       
+    consumables: `meals`,
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'   
 }, {
     id: 3,
     name: 'radio',
     render: true,
-    description: 'Battery-Powered (or hand crank) Radio'        
+    description: 'Battery-Powered (or hand crank) Radio',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'     
 }, {
     id: 4,
     name: 'flashlight',
     render: true,
-    description: 'Flashlight'        
+    description: 'Flashlight',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 5,
     name: 'first-aid',
     render: true,
-    description: 'First-Aid Kit'        
+    description: 'First-Aid Kit',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 6,
     name: 'shoes',
     render: true,
-    description: 'Pair of Shoes',     
+    description: 'Pair of Shoes',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'     
 }, {
     id: 7,
     name: 'whistle',
     render: true,
-    description: 'Whistle'        
+    description: 'Whistle',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 8,
     name: 'batteries',
     render: true,
-    description: 'Extra Batteries'        
+    description: 'Extra Batteries',        
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'
 }, {
     id: 9,
     name: 'dust-mask',
@@ -76,42 +83,50 @@ export const neededItems = [{
     description: `Have 1 dust mask per person`,
     amountPerPerson: 1,
     numDays: 1,
-    consumables: 'dust-masks',      
+    consumables: 'dust-masks',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'      
 }, {
     id: 10,
     name: 'sheeting',
     render: true,
-    description: 'Plastic Sheeting'        
+    description: 'Plastic Sheeting',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 11,
     name: 'duct-tape',
     render: true,
-    description: 'Duct Tape'        
+    description: 'Duct Tape',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 12,
     name: 'wipes',
     render: true,
-    description: 'Moist Towelettes, Garbage Bags, Plastic Ties'        
+    description: 'Moist Towelettes, Garbage Bags, Plastic Ties',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 13,
     name: 'tools',
     render: true,
-    description: 'Wrench and/or Pliers'        
+    description: 'Wrench and/or Pliers',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 14,
     name: 'can-opener',
     render: true,
-    description: 'Manual Can Opener'        
+    description: 'Manual Can Opener',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
 }, {
     id: 15,
     name: 'map',
     render: true,
-    description: 'Local Maps'        
+    description: 'Local Maps',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'    
 }, {
     id: 16,
     name: 'cell-phone',
     render: true,
-    description: 'Cell phone (with chargers and a backup battery)'        
+    description: 'Cell phone (with chargers and a backup battery)',
+    link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'    
 }];
 
 export const suggestedItems = [
@@ -119,19 +134,22 @@ export const suggestedItems = [
         id: 101,
         name: 'eyewear',
         render: false,
-        description: 'Prescription eyeglasses and contact lens solution'        
+        description: 'Prescription eyeglasses and contact lens solution',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'      
     },
     {
         id: 102,
         name: 'baby',
         render: false,
-        description: 'Infant formula, bottles, diapers, wipes and diaper rash cream'        
+        description: 'Infant formula, bottles, diapers, wipes and diaper rash cream',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'       
     },
     {
         id: 103,
         name: 'transportation',
         render: false,
-        description: 'links to additional transportation'        
+        description: 'links to additional transportation',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 104,
@@ -140,50 +158,58 @@ export const suggestedItems = [
         description: `Have 1 mask per person (for everyone ages 2 and above)`,
         amountPerPerson: 1,
         numDays: 1,
-        consumables: 'masks', 
+        consumables: 'masks',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4' 
        
     },
     {
         id: 105,
         name: 'disinfectants',
         render: true,
-        description: 'Soap, hand sanitizer, disinfecting wipes to disinfect surfaces'        
+        description: 'Soap, hand sanitizer, disinfecting wipes to disinfect surfaces',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 106,
         name: 'medications',
         render: false,
-        description: 'Prescription medications'        
+        description: 'Prescription medications',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 107,
         name: 'otc-meds',
         render: true,
-        description: 'Non-prescription medications such as pain relievers, anti-diarrhea medication, antacids or laxatives'        
+        description: 'Non-prescription medications such as pain relievers, anti-diarrhea medication, antacids or laxatives',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 108,
         name: 'pet',
         render: false,
-        description: 'Pet food and extra water for your pet'        
+        description: 'Pet food and extra water for your pet',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 109,
         name: 'cash',
         render: true,
-        description: `Cash or traveler's checks`        
+        description: `Cash or traveler's checks`,
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 110,
         name: 'documents',
         render: true,
-        description: `Important family documents such as copies of insurance policies, identification and bank account records saved electronically or in a waterproof, portable container`   
+        description: `Important family documents such as copies of insurance policies, identification and bank account records saved electronically or in a waterproof, portable container`,
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'   
     },
     {
         id: 111,
         name: 'blankets',
         render: true,
-        description: 'Sleeping bag or warm blanket for each person'        
+        description: 'Sleeping bag or warm blanket for each person',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 112,
@@ -192,43 +218,50 @@ export const suggestedItems = [
         description: `Have 1 change of clothing per person appropriate for your climate`,
         amountPerPerson: 1,
         numDays: 1,
-        consumables: 'sets',       
+        consumables: 'sets',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'       
     },
     {
         id: 113,
         name: 'extinguisher',
         render: true,
-        description: 'Fire extinguisher'        
+        description: 'Fire extinguisher',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 114,
         name: 'matches',
         render: true,
-        description: 'Matches in a waterproof container'        
+        description: 'Matches in a waterproof container',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 115,
         name: 'hygiene',
         render: true,
-        description: 'Menstrual supplies and personal hygiene items'        
+        description: 'Menstrual supplies and personal hygiene items',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 116,
         name: 'mess-kit',
         render: true,
-        description: 'Mess kits, paper cups, plates, paper towels and plastic utensils'        
+        description: 'Mess kits, paper cups, plates, paper towels and plastic utensils',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'       
     },
     {
         id: 117,
         name: 'writing',
         render: true,
-        description: 'Paper and pencil'        
+        description: 'Paper and pencil',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'        
     },
     {
         id: 118,
         name: 'games',
         render: true,
-        description: 'Books, games, puzzles, or other activities',       
+        description: 'Books, games, puzzles, or other activities',
+        link: 'https://www.amazon.com/Blue-Gallon-Water-Storage-WaterPrepared/dp/B0785BHYFG/ref=sr_1_4?dchild=1&keywords=water+storage&qid=1631822972&sr=8-4'       
     }    
-    // add transportation
+    // add transportation (someone already marked 103 as transportation?)
 ];
