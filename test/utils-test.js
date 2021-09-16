91fef143b71184af5afc3855ec55680c5fee1ada
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-16 10:41:58 -0700

    add utils testing

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 
-import { setUser, USER } from '../universal/local-storage-utils.js';
+import { setUser, getUser, USER, getEssentialsList, setEssentials } from '../universal/local-storage-utils.js';
 
 
 const test = QUnit.test;
@@ -24,4 +24,56 @@ test('setUser should set a user object into localStorage', (expect) => {
     const actual = JSON.parse(localStorage.getItem(USER));
 
     expect.deepEqual(actual, testUser);
+});
+
+test('getUser should get the user object from localStorage', (expect) => {
+    localStorage.setItem(USER, JSON.stringify(testUser));
+
+    const actual = getUser();
+
+    expect.deepEqual(actual, testUser);
+});
+
+test('getEssentials should get the list of Needed Items from localStorage', (expect) => {
+    localStorage.setItem(USER, JSON.stringify(testUser));
+
+    const actual = getEssentialsList();
+
+    expect.deepEqual(actual, testUser.itemsNeeded);
+});
+
+test('setEssentials should set the to do items in localStorage', (expect) => {
+    localStorage.setItem(USER, JSON.stringify(testUser));
+
+    const neededItems = [
+        {
+            id: 1,
+            name: 'water',
+            render: true,
+            description: `Have 1 gallon of water per person on reserve for up to 3 days`,
+            amountPerPerson: 1,
+            numDays: 3,
+            consumables: `gallons`,
+        
+        }, {
+            id: 2,
+            name: 'food',
+            render: true,
+            description: `Have 3 meals per person for up to 3 days`,
+            amountPerPerson: 3,
+            numDays: 3,
+            consumables: `meals`,       
+        }, {
+            id: 3,
+            name: 'radio',
+            render: true,
+            description: 'Battery-Powered (or hand crank) Radio'        
+        }
+    ];
+
+    setEssentials(neededItems);
+
+    const updatedUserObj = JSON.parse(localStorage.getItem(USER));
+
+    expect.deepEqual(updatedUserObj.itemsNeeded, neededItems);
 });
