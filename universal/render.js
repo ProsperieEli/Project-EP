f593bea5fa4baf652c93f06ac8e8e24a567fecf3
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-09-17 09:44:57 -0700

    fix rendering function

--- universal/render.js
+++ universal/render.js
@@ -1,15 +1,18 @@
-import { setEssentials, setSuggested } from '../universal/local-storage-utils.js';
+import { getUser, setEssentials, setSuggested } from '../universal/local-storage-utils.js';
 export { renderEssentialsNeeded, renderSuggestedNeeded, renderLinks };
+
 
 function renderEssentialsNeeded(essentialItems){
     const essentialsUL = document.getElementById('essential-ul');
+
     essentialsUL.innerHTML = '';
 
     for (let item of essentialItems) {
 
+        const user = getUser();
         const listItemEl = document.createElement('li');
         const listItem = document.createElement('p');
-        
+
         listItemEl.classList.add('list-itemEl');
         listItem.classList.add('list-item');
 
@@ -18,7 +21,12 @@ function renderEssentialsNeeded(essentialItems){
         }
 
         listItem.textContent = item.description;
-
+        //concat a sentence with a findbydescription/id function and add in backticks/literals for totalfamily, totalGoods of the id.name
+        //go in render function on prep page
+        if (item.amountPerPerson) {
+            const totalNeeded = Number((item.amountPerPerson * user.totalFamily) * item.numDays);
+            listItem.textContent = item.description + `. You have ${user.totalFamily} members in your household. You will need ${totalNeeded} ${item.consumables}. `;
+        }
         listItem.addEventListener('click', () => {
             // change to completed = true when item clicked (!user.completed) classList.toggle
             item.completed = (!item.completed); //toggle the completed state when an item is clicked
@@ -29,7 +37,7 @@ function renderEssentialsNeeded(essentialItems){
         listItemEl.append(listItem);
         essentialsUL.append(listItemEl);
     }
-    
+
 }
 
 function renderSuggestedNeeded(suggestedItems){
@@ -37,19 +45,27 @@ function renderSuggestedNeeded(suggestedItems){
     suggestedUL.innerHTML = '';
 
     for (let item of suggestedItems) {
-  
+
         if (item.render === true) {
+            const user = getUser();
             const listItemEl = document.createElement('li');
             const listItem = document.createElement('p');
-        
+
             listItemEl.classList.add('list-itemEl');
             listItem.classList.add('list-item');
 
             if (item.completed === true){
                 listItem.classList.add('completed-item');
             }
+        //concat a sentence with a findbydescription/id function and add in backticks/literals for totalfamily, totalGoods of the id.name
+        //go in render function on prep page
 
             listItem.textContent = item.description;
+
+            if (item.amountPerPerson) {
+                const totalSuggested = (item.amountPerPerson * user.totalFamily) * item.numDays;
+                listItem.textContent = item.description + `. You have ${user.totalFamily} members in your household. You will need ${totalSuggested} ${item.consumables}. `;
+            }
 
             listItem.addEventListener('click', () => {
                 // change to completed = true when item clicked (!user.completed) classList.toggle
